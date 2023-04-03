bd601f5bf02ff19904077b5155dc159247f5c19a
Zach <zachlandis91@gmail.com>
2023-04-03 12:30:14 -0600

    commit tree 1e4d3ca8db9ce9c2ece8c97f15b96279cdd12e92

--- methods.rb
+++ methods.rb
@@ -1,1 +1,28 @@
-# Your code here!
+def greet_programmer
+    puts "Hello, programmer!"
+end
+
+greet_programmer
+
+
+def greet (name)
+    puts "Hello, #{name}!"
+end
+
+
+def greet_with_default(name = "programmer") 
+    puts "Hello, #{name}!"
+end
+
+
+def add(num1, num2)
+    return num1 + num2
+end
+
+def halve(num) 
+    if num.class != Integer
+        return nil
+    end
+
+    return num / 2
+end
